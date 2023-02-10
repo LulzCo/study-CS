cc86c9ca614dd7b9247809d33a19891e84417bab
LulzCo <ysw991106@gmail.com>
2023-02-10 20:53:19 +0900

    test

--- dataAlgorithm/src/main/java/chap01/Median.java
+++ dataAlgorithm/src/main/java/chap01/Median.java
@@ -3,6 +3,8 @@ package chap01;
 public class Median {
     public static void main(String[] args) {
         System.out.println("dfdsfadsfsad");
-
+        System.out.println("dfdsfadsfsad");
+        System.out.println("dfdsfadsfsad");
+        System.out.println("dfdsfadsfsad");
     }
 }
