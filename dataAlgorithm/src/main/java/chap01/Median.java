97a31d0d4b391b2317c5bbc6512549625a6c02aa
LulzCo <ysw991106@gmail.com>
2023-02-10 20:50:21 +0900

    ignore 수정

--- dataAlgorithm/src/main/java/chap01/Median.java
+++ dataAlgorithm/src/main/java/chap01/Median.java
@@ -2,7 +2,7 @@ package chap01;
 
 public class Median {
     public static void main(String[] args) {
-        System.out.println("test");
-        
+        System.out.println("testfdasfasdfads");
+
     }
 }
