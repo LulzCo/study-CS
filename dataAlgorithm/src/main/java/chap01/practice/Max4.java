6bbc542b3a39762806f4b0fd6bb97539d188353b
LulzCo <ysw991106@gmail.com>
2023-02-11 16:53:39 +0900

    최댓값 연습 문제

---
+++ dataAlgorithm/src/main/java/chap01/practice/Max4.java
@@ -0,0 +1,15 @@
+package chap01.practice;
+
+public class Max4 {
+    static int max4(int a, int b, int c, int d) {
+        int max = a;
+        if (b >= max) max = b;
+        if (c >= max) max = c;
+        if (d >= max) max = d;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max4(1,2,3,4));
+    }
+}
